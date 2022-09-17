cd878c895ddd23b124af582cfff2771e11d9d957
wlsdn9297 <wlsdn9297@naver.com>
2022-09-17 13:00:15 +0900

    곱하기 혹은 더하기

---
+++ src/main/kotlin/Greedy_Algorithm/곱하기 혹은 더하기.kt
@@ -0,0 +1,17 @@
+package Greedy_Algorithm
+
+fun main() {
+    var data = readLine()!!
+    var result = 0
+
+    for(i in data) {
+//        var num = i - '0'
+        var num = i.digitToInt()
+        if (num <= 1 || result <= 1) {
+            result += num
+        } else {
+            result *= num
+        }
+    }
+    print(result)
+}
