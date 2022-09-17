1201f9a604bbce8a9eedaed086780a5dad334be4
wlsdn9297 <wlsdn9297@naver.com>
2022-09-17 12:11:47 +0900

    거스름 돈

---
+++ src/main/kotlin/Greedy_Algorithm/거스름 돈.kt
@@ -0,0 +1,16 @@
+package Greedy_Algorithm
+
+fun main() {
+    var n = readLine()!!.toInt()
+
+    // 큰 단위의 화폐부터 차례대로 확인하기
+    var array = intArrayOf(500, 100, 50, 10)
+    var count = 0
+
+    for(coin in array) {
+        // 해당 화폐로 거슬러 줄 수 있는 동전의 개수 세기
+       count += n / coin
+       n %= coin
+    }
+    print(count)
+}
